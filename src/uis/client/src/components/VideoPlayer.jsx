e50c54d44e7e5c694c2321cc925c42dd9222e723
alex-chemis <4lxchemis@gmail.com>
2024-05-26 22:39:32 +0300

    Add video player

--- src/uis/client/src/components/VideoPlayer.jsx
+++ src/uis/client/src/components/VideoPlayer.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import ShakaPlayer from 'shaka-player-react';
+import { useState } from 'react'
+import ReactPlayer from 'react-player';
 import 'shaka-player/dist/controls.css';
 import './styles.css'; // Import your CSS file
 
 const VideoPlayer = ({ mpdUrl }) => {
-    return (
-        <div className="fixed-player-size">
-            <ShakaPlayer autoPlay src={mpdUrl} />
+    const [hlsUrl, setHlsUrl] = useState(
+        "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      );
+
+      return (
+        <div className="row justify-content-center">
+          <input
+            type="text"
+            className="form-control w-90 m-2 my-4"
+            placeholder="HLS Url..."
+            value={hlsUrl}
+            aria-label="hls-url"
+            aria-describedby="set-hls-url"
+            onChange={(e) => setHlsUrl(e.target.value)}
+          />
+          <ReactPlayer
+            url={hlsUrl}
+            controls={true}
+            width="60%"
+            height="auto"
+          />
         </div>
-    );
+      );
 };
 
 export default VideoPlayer;
