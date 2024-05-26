12a5c3f130aa9c10ba355046c5992615d02d3d1e
alex-chemis <4lxchemis@gmail.com>
2024-05-26 18:01:32 +0300

    Complete player

--- src/microservices/S3ApiMicroservice/Controllers/FilesController.cs
+++ src/microservices/S3ApiMicroservice/Controllers/FilesController.cs
@@ -70,13 +70,4 @@ public class FilesController(IAmazonS3 s3Client) : ControllerBase
         await s3Client.DeleteObjectAsync(bucketName, key);
         return NoContent();
     }
-
-    [HttpGet("{bucketName}/{key}")]
-    public async Task<IActionResult> Play(string bucketName, string key)
-    {
-        var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(s3Client, bucketName);
-        if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
-        var s3Object = await s3Client.GetObjectAsync(bucketName, key);
-        return File(s3Object.ResponseStream, s3Object.Headers.ContentType);
-    }
 }
